e930c7d9363b9e18c2dbeed0641c585cdbde7c3e
Khanh ngu yen <khanhntqgcd210526@fpt.edu.vn>
2023-11-23 10:38:32 +0700

    Add Model(Category) Add SomeMigration for Category in DBContext

--- BookStore/Data/BookAppDBContext.cs
+++ BookStore/Data/BookAppDBContext.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Data
 {
     public class BookAppDBContext : DbContext
     {
+        DbSet<Category> Categories { get; set; }
         public BookAppDBContext(DbContextOptions<BookAppDBContext> options) : base(options) {
         
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Horror", Description="This is so scary", DisplayOrder=2},
+                new Category { Id=2, Name="Action", Description="Fighting to ", DisplayOrder=3},
+                new Category { Id=3, Name="Roman", Description="Kissing under the sunset", DisplayOrder=1},
+                new Category { Id=4, Name = "Science", Description = "Research to save the earth", DisplayOrder =4}
+                );
         }
     }
 }
---
+++ BookStore/Migrations/20231123032628_AddCategorySeedingToCategoriesTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategorySeedingToCategoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, "This is so scary", 2, "Horror" },
+                    { 2, "Fighting to ", 3, "Action" },
+                    { 3, "Kissing under the sunset", 1, "Roman" },
+                    { 4, "Research to save the earth", 4, "Science" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
